7d08a73c4760a065fe35ec922667437d9e11c397
Tushar <tusharkumar0807@gmail.com>
2022-07-08 16:44:04 +0530

    add another car

--- core.js
+++ core.js
@@ -5,25 +5,25 @@ const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 50, 80, "KEYS");
 const traffic = [
-    new Car(road.getLaneCenter(1), -100, 50, 80, "DUMMY")
+    new Car(road.getLaneCenter(1), -100, 50, 80, "DUMMY", 4)
 ];
 
 animate();
 
 function animate() {
     for (let i = 0; i < traffic.length; i++) {
-        traffic[i].update(road.borders);
+        traffic[i].update(road.borders, []);
     }
-    car.update(road.borders);
+    car.update(road.borders, traffic);
 
     canvas.height = window.innerHeight;
     ctx.save();
     ctx.translate(0, -car.y + canvas.height * 0.8);
     road.draw(ctx);
     for (let i = 0; i < traffic.length; i++) {
-        traffic[i].draw(ctx);
+        traffic[i].draw(ctx,"black");
     }
-    car.draw(ctx);
+    car.draw(ctx, "blue");
 
     ctx.restore();
     requestAnimationFrame(animate);
